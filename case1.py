a04c495f07ddec18434a0d2e37c981a66b85b469
Vladislav Petrov <vlad_petrov01@mail.ru>
2019-09-21 11:25:16 +0700

    Work!

--- case1.py
+++ case1.py
@@ -7,7 +7,26 @@ import turtle
 
 
 
-def
+def Big_red_Triangle():
+    #TODO (Nastya) Big red Triangle
+
+def orange_Square():
+    #TODO (Alina) orange Square
+
+def Small_purple_Triangle():
+    #TODO (Vlad) Small purple Triangle
+
+def Big_yellow_Triangle():
+    #TODO (Nastya) Big yellow Triangle
+
+def Blue_Triangle():
+    #TODO (Alina) Blue Triangle
+
+def Green_Parallelogram():
+    #TODO (Vlad) Green Parallelogram
+
+def Small_pink_Triangle():
+    #TODO (Vlad) small pink Triangle
 
 
 
