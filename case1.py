c2c4c770ec3f55f2ae589fe1a9c9c437924f3fe8
AlinaDaniel <alina.daniel66@icloud.com>
2019-09-23 02:35:02 +0300

    Spaceship, happy human, cock are done.

--- case1.py
+++ case1.py
@@ -142,17 +142,66 @@ def small_pink_triangle(x, y, a):
     turtle.pendown()
     turtle.end_fill()
 
+
+def cock() :
+    # TODO (Alina) Print a cock.
+    turtle.right(45)
+    blue_triangle(-100, 200, 90)
+    turtle.left(135)
+    turtle.color("red")
+    big_red_triangle(-10, 100, 180)
+    orange_square(90, 10, 90)
+    turtle.right(180)
+    turtle.color("yellow")
+    big_yellow_triangle(0, 0, 180)
+    turtle.left(135)
+    b = (2 ** 0.5) * 90
+    c = b / 2
+    small_pink_triangle((90 + c), (110 + c), 90)
+    small_purple_triangle((20 + c), (-170 + c), 90)
+    turtle.right(180)
+    green_parallelogram(-190, 90, 90)
+
+def happy_human() :
+    # TODO (Alina) Print a happy human.
+    turtle.right(45)
+    orange_square(0, 280, 90)
+    turtle.right(135)
+    turtle.color("red")
+    b = (2 ** 0.5) * 90
+    big_red_triangle(-5, (270 - b), 180)
+    turtle.left(180)
+    turtle.color("yellow")
+    big_yellow_triangle(5, (270 - b), 180)
+    turtle.right(90)
+    blue_triangle(5, (80 - 2 * b), 90)
+    turtle.left(90)
+    green_parallelogram(-5, (80 - b), 90)
+    turtle.left(135)
+    small_pink_triangle(-65, (-45 - 2 * b), 90)
+    d = b * 2 / 3
+    small_purple_triangle((95 + d), (70 - 2 * b), 90)
+
+
+def spaceship() :
+    #TODO (Alina) Print a spaceship.
+    turtle.right(45)
+    small_pink_triangle(0, 280, 90)
+    turtle.left(225)
+    b = (2 ** 0.5) * 90
+    c = b / 2
+    blue_triangle(c, (270 - c), 90)
+    turtle.color("yellow")
+    turtle.right(225)
+    big_yellow_triangle((c - 5), (260 - c - b), 180)
+    turtle.right(90)
+    turtle.color("red")
+    big_red_triangle((c - b), (250 - c - b - b), 180)
+    orange_square((c + c - b - 5), (240 - c - c - b - b), 90)
+    small_purple_triangle((c - b - 10), (230 - b - c - b - b), 90)
+    green_parallelogram((c + 10), (240 - c - b - b), 90)
+
 def main():
-        turtle.color("red")
-        big_red_triangle(-200, 100, 180)
-        orange_square(0, 0, 180)
-        blue_triangle(100, 0, 180)
-        small_pink_triangle(300, 0, 180)
-        green_parallelogram(0, -100, 180)
-        small_purple_triangle(200, -100, 180)
-        turtle.color("yellow")
-        big_yellow_triangle(-100, 50, 180)
-        turtle.done()
 
 main()
 
