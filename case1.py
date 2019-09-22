bbc6c3ee3aba1d00459a078080d0ca0ef0e5fba9
Vladislav Petrov <vlad_petrov01@mail.ru>
2019-09-22 17:51:59 +0700

    keis.

--- case1.py
+++ case1.py
@@ -22,7 +22,7 @@ def big_red_triangle(x, y, a) :
     turtle.left(135)
     turtle.forward(b)
     turtle.end_fill()
-
+    pass
 
 def orange_square(x, y, a) :
     # TODO (Alina) Function, drawing orange square.
@@ -32,19 +32,35 @@ def orange_square(x, y, a) :
     turtle.color('orange')
     turtle.fillcolor('orange')
     turtle.begin_fill()
-    turtle.forward(a)
+    turtle.fd(a)
     turtle.right(90)
-    turtle.forward(a)
+    turtle.fd(a)
     turtle.right(90)
-    turtle.forward(a)
+    turtle.fd(a)
     turtle.right(90)
-    turtle.forward(a)
+    turtle.fd(a)
     turtle.right(90)
     turtle.pendown()
     turtle.end_fill()
+ pass
 
-def small_purple_triangle():
-    #TODO (Vlad) Small purple Triangle
+def small_purple_triangle(x, y, a):
+    #TODO (Vlad) Function, drawing small purple triangle
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.down()
+    turtle.color('purple')
+    turtle.fillcolor('purple')
+    turtle.begin_fill()
+    b = (2 ** 0.5) * a
+    turtle.forward(a)
+    turtle.right(135)
+    turtle.forward(b)
+    turtle.right(135)
+    turtle.forward(a)
+    turtle.pendown()
+    turtle.end_fill()
+pass
 
 
 def big_yellow_triangle(x, y, a):
@@ -63,6 +79,7 @@ def big_yellow_triangle(x, y, a):
     turtle.left(135)
     turtle.forward(b)
     turtle.end_fill()
+pass
 
 
 def blue_triangle(x, y, a) :
@@ -81,15 +98,50 @@ def blue_triangle(x, y, a) :
     turtle.forward(b)
     turtle.pendown()
     turtle.end_fill()
+pass
 
 
-def green_parallelogram():
-    #TODO (Vlad) Green Parallelogram
+def green_parallelogram(x, y, a):
+    #TODO (Vlad) Function, drawing Green Parallelogram
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.down()
+    turtle.color('green')
+    turtle.fillcolor('green')
+
+    b = (2 ** 0.5) * a
+
+    turtle.begin_fill()
+    turtle.fd(b)
+    turtle.right(30)
+    turtle.fd(a)
+    turtle.right(60)
+    turtle.fd(b)
+    turtle.right(150)
+    turtle.fd(a)
+    turtle.pendown()
+    turtle.end_fill()
+
+pass
 
 
-def small_pink_triangle():
-    #TODO (Vlad) small pink Triangle
-
+def small_pink_triangle(x, y, a):
+    #TODO (Vlad) Function, drawing small pink Triangle
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.down()
+    turtle.color('pink')
+    turtle.fillcolor('pink')
+    turtle.begin_fill()
+    b = (2 ** 0.5) * a
+    turtle.forward(a)
+    turtle.right(135)
+    turtle.forward(b)
+    turtle.right(135)
+    turtle.forward(a)
+    turtle.pendown()
+    turtle.end_fill()
+pass
 
     def main():
         turtle.color("red")
@@ -101,4 +153,5 @@ def small_pink_triangle():
         turtle.done()
 
     main()
+
 
