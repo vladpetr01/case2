c9e4b2801424d8c8cc84d9d9efb51cf773c486aa
AlinaDaniel <alina.daniel66@icloud.com>
2019-09-22 12:23:41 +0300

    Function for blue triangle and orange square is done.

--- case1.py
+++ case1.py
@@ -1,16 +1,22 @@
 #First case
 # Developers:   Zemtseva A. (20%),
 #               Petrov V. (60%),
-#               . (30%)
+#               Daniel A. (30%)
 import turtle
 
 
 
+def big_red_triangle() :
+    #TODO (Nastya) Big red Triangle
 
-def square(x, y, a,):
+def orange_square(x, y, a) :
+    # TODO (Alina) Function, drawing orange square.
     turtle.up()
     turtle.setposition(x, y)
     turtle.down()
+    turtle.color('orange')
+    turtle.fillcolor('orange')
+    turtle.begin_fill()
     turtle.forward(a)
     turtle.right(90)
     turtle.forward(a)
@@ -19,14 +25,38 @@ def square(x, y, a,):
     turtle.right(90)
     turtle.forward(a)
     turtle.right(90)
+    turtle.pendown()
+    turtle.end_fill()
+
+def small_purple_triangle():
+    #TODO (Vlad) Small purple Triangle
+
+def big_yellow_triangle():
+    #TODO (Nastya) Big yellow Triangle
 
 
-def main():
-    square(-200, 200,180)
-    square(20,200, 180)
-    square(20,-20, 180)
-    square(-200,-20,180)
-    turtle.done()
+def blue_triangle(x, y, a) :
+    #TODO (Alina) Function, drawing blue triangle.
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.down()
+    turtle.color('light blue')
+    turtle.fillcolor('light blue')
+    turtle.begin_fill()
+    b = (2 ** 0.5) * a
+    turtle.forward(b)
+    turtle.right(135)
+    turtle.forward(2 * a)
+    turtle.right(135)
+    turtle.forward(b)
+    turtle.pendown()
+    turtle.end_fill()
 
-main()
+
+def green_parallelogram():
+    #TODO (Vlad) Green Parallelogram
+
+def small_pink_triangle():
+    #TODO (Vlad) small pink Triangle
+
 
